28356ff3366e1f62bb98286ea8952057d91aa2ea
Jake Soenneker <jake@soenneker.com>
2025-09-14 14:16:02 -0500

    Initial commit

---
+++ src/ColorType.cs
@@ -0,0 +1,22 @@
+﻿using Intellenum;
+
+namespace Soenneker.Quark.Enums.ColorTypes;
+
+/// <summary>
+/// Common html theme and color types in enumeration form.
+/// </summary>
+[Intellenum<string>]
+public sealed partial class ColorType
+{
+    public static readonly ColorType Default = new(null);
+    public static readonly ColorType Primary = new("primary");
+    public static readonly ColorType Secondary = new("secondary");
+    public static readonly ColorType Success = new("success");
+    public static readonly ColorType Danger = new("danger");
+    public static readonly ColorType Warning = new("warning");
+    public static readonly ColorType Info = new("info");
+    public static readonly ColorType Light = new("light");
+    public static readonly ColorType Dark = new("dark");
+    public static readonly ColorType Link = new("link");
+    public static readonly ColorType Muted = new("muted");
+}
---
+++ test/Soenneker.Quark.Enums.ColorTypes.Tests/ColorTypeTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.ColorTypes.Tests;
+
+public sealed class ColorTypeTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
